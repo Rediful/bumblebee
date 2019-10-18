d3428a0c29d9d4a056ecf831d50a5b07b8aec088
halvin shen <328076475@qq.com>
2019-10-18 17:28:48 +0800

    styled component (#6)
    * styled component
    
    * drop css

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,6 +4,8 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import ProTip from '../../components/ProTip';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 
 function Copyright() {
   return (
@@ -19,12 +21,23 @@ function Copyright() {
 }
 
 const App: React.FC = () => {
+  const StyledButton = styled(Button)`
+    background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+    border-radius: 3px;
+    border: 0;
+    color: white;
+    height: 48px;
+    padding: 0 30px;
+    box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+  `;
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example with TypeScript
         </Typography>
+        <StyledButton>Styled Button</StyledButton>
         <ProTip />
         <Copyright />
       </Box>
